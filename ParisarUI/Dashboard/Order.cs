d0944563d16067b19c0c70a0ecb7c301a48683ce
chary-vk <vchary43@gmail.com>
2026-04-09 18:07:16 +0530

    Add project files.

---
+++ ParisarUI/Dashboard/Order.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ParisarUI.Dashboard
+{
+    public class Order
+    {
+        public Guid OrderId { get; set; } = Guid.NewGuid();
+
+        public required string CustomerName { get; set; }
+
+        public required string Country { get; set; }
+
+        public DateTimeOffset OrderDate { get; set; }
+
+        public OrderType OrderType { get; set; }
+
+        public decimal GrossValue { get; set; }
+
+        public decimal DiscountPercentage { get; set; }
+
+        public decimal NetValue => GrossValue * (1 - (DiscountPercentage / 100));
+    }
+
+    public enum OrderType
+    {
+        Web, Contract, Mail, Phone
+    }
+}
---
+++ ParisarUI/Dashboard/SampleData.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParisarUI.Dashboard
+{
+    public static class SampleData
+    {
+        public static List<Order> GetOrders()
+        {
+            return new List<Order>
+            {
+                new Order
+                {
+                    CustomerName = "John",
+                    Country = "India",
+                    OrderDate = DateTimeOffset.Now,
+                    OrderType = OrderType.Web,
+                    GrossValue = 1000,
+                    DiscountPercentage = 10
+                },
+                new Order
+                {
+                    CustomerName = "David",
+                    Country = "USA",
+                    OrderDate = DateTimeOffset.Now,
+                    OrderType = OrderType.Contract,
+                    GrossValue = 2000,
+                    DiscountPercentage = 5
+                },
+                new Order
+                {
+                    CustomerName = "Ravi",
+                    Country = "India",
+                    OrderDate = DateTimeOffset.Now,
+                    OrderType = OrderType.Phone,
+                    GrossValue = 1500,
+                    DiscountPercentage = 8
+                },
+                new Order
+                {
+                    CustomerName = "Sara",
+                    Country = "UK",
+                    OrderDate = DateTimeOffset.Now,
+                    OrderType = OrderType.Mail,
+                    GrossValue = 1800,
+                    DiscountPercentage = 12
+                }
+            };
+        }
+    }
+}
