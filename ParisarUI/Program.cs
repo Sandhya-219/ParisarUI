1b80ac31c4ce19d26619b5964a86adaa9c9b3016
chary-vk <vchary43@gmail.com>
2026-04-10 17:52:15 +0530

    10/04/2026 - Created bar chart and line chart

--- ParisarUI/Program.cs
+++ ParisarUI/Program.cs
@@ -6,6 +6,7 @@ using ParisarUI;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddApexCharts();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
